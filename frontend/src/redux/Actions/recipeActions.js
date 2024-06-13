918b8a54481de8d4ac07cac359149b2ee71766a4
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-06-13 18:45:39 +0530

    feat:Reducer actions created

---
+++ frontend/src/redux/Actions/recipeActions.js
@@ -0,0 +1,24 @@
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const DELETE_RECIPE = 'DELETE_RECIPE';
+export const UPDATE_RECIPE = 'UPDATE_RECIPE';
+
+export const addRecipe = (recipe) => {
+    return {
+        type: ADD_RECIPE,
+        payload: recipe
+    }
+}
+
+export const deleteRecipe = (recipeId) => {
+    return {
+        type: DELETE_RECIPE,
+        payload: recipeId
+    }
+}
+
+export const updateRecipe = (recipe) => {
+    return {
+        type: UPDATE_RECIPE,
+        payload: recipe
+    }
+}
