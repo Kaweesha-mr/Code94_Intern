bfd57b6057b567763f10ae39d5dd63764b8e3d05
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-06-14 06:38:03 +0530

    ref: cors applied for all urls

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,17 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 const recipeRoutes = require('./routes/recipeRoutes.routes');
 const errorHandler = require('./utils/errorHandler');
+const cors = require('cors');
+
 
 require('dotenv').config();
 
 const app = express();
+
+//allow from localhost:3000 and localhost:4000
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:4000']
+}));
 
 // Connect to MongoDB
 connectDB();
@@ -22,7 +29,7 @@ app.use('/api/recipes', recipeRoutes);
 // Error Handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
