625484c1799754c837c7fb7daa412e42a0ef4f1f
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-06-14 07:05:28 +0530

    fix: same recipe duplicating error fixed

--- frontend/src/redux/Reducer/recipeReducer.js
+++ frontend/src/redux/Reducer/recipeReducer.js
@@ -1,18 +1,12 @@
-import { ADD_RECIPE, DELETE_RECIPE, UPDATE_RECIPE,FETCH_RECIPES } from "../Actions/recipeActions";
+// src/redux/reducers/recipeReducer.js
+import { ADD_RECIPE, DELETE_RECIPE, UPDATE_RECIPE, FETCH_RECIPES } from "../Actions/recipeActions";
 
-const initialState = [
-    
-];
+const initialState = [];
 
 const recipeReducer = (state = initialState, action) => {
-
-    console.log(action.payload);
     switch (action.type) {
         case ADD_RECIPE:
-            return [
-                ...state,
-                action.payload
-            ];
+            return [...state, action.payload];
         case DELETE_RECIPE:
             return state.filter(recipe => recipe.id !== action.payload);
         case UPDATE_RECIPE:
@@ -20,16 +14,12 @@ const recipeReducer = (state = initialState, action) => {
                 recipe.id === action.payload.id ? action.payload : recipe
             );
         case FETCH_RECIPES:
-            const newRecipes = action.payload.map(recipe => ({
+            // Directly return the fetched recipes as the new state
+            return action.payload.map(recipe => ({
                 id: recipe._id, // Use the MongoDB _id as the id
                 title: recipe.title,
                 description: recipe.description
             }));
-
-            // Combine the received recipes with the existing initialState array
-            const mergedRecipes = [...state, ...newRecipes];
-            return mergedRecipes;
-    
         default:
             return state;
     }
